0347f5448230dda33194ba8f6c003666e1a01d4f
Cassidye <cassidyroberts074@gmail.com>
2025-05-08 01:31:38 -0700

    ah

---
+++ isle/script/globals.gd
@@ -0,0 +1,14 @@
+extends Node
+#0 - name - str
+#1 - base health - int
+#2 - base damage - int
+#3 - description - str
+#4 - ability name - str
+#5 - type (bug etc.) - str
+#6 - location acquired - str
+#7 - rarity - str
+#8 - type (support, etc.) - str
+#9 - cost - int
+var all_cards = {"Rat": ["Rat",1,1,"No ability\n*Skitter*","None","Mammal","Rat bog","Common","Creature",0],
+"Dragonfly" : ["Dragonfly",1,1,"Breathes fire on three spaces in front of it\nBlank","Fire","Bug","Lake","Uncommon","Creature",1],
+"The Rats" : ["The Rats",null,null,"Doubles the number of rat cards in play\nThey prey at night, they stalk at night, they're the rats","Rat Swarm","N/A","Rat King","Boss","Support",0]}
