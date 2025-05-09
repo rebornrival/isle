849666c22a41143449e5c262e98645d3c3f148cc
rebornrival <renee@devivo.com>
2025-05-08 18:47:58 -0700

    Added isopd and globro

---
+++ isle/script/card.gd
@@ -0,0 +1,24 @@
+extends Node2D
+var card_stats
+#0 - name - str
+#1 - base health - int
+#2 - base damage - int
+#3 - description - str
+#4 - ability name - str
+#5 - type (bug etc.) - str
+#6 - location acquired - str
+#7 - rarity - str
+#8 - type (support, etc.) - str
+#9 - cost - int
+func _physics_process(delta: float) -> void:
+	pass
+
+func initialize(card_info):
+	card_stats = card_info
+	$name.text = card_info[0]
+	if card_info[1] != null:
+		$health_and_damage.text = str(card_info[1])+' : '+str(card_info[2])
+	else:
+		$health_and_damage.text = "Support"
+	$cost.text = "Cost "+str(card_info[9])
+	$ability_and_description.text = card_info[3]
