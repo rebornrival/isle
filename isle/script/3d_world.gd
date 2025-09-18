1bffc19f9b885d0027e2f006d170082f5f018909
Cassidy Roberts <cassidyroberts074@gmail.com>
2025-09-18 14:15:28 -0700

    more work on card instances

--- isle/script/3d_world.gd
+++ isle/script/3d_world.gd
@@ -1,11 +1,26 @@
 extends Node3D
-var deck = ["Rat","Dragonfly","The Rats","Rat","Rat","Rat","Dragonfly"]
+var deck = ["Rat","Gleebus","The Rats","Rat","Rat","Rat","Glorbo","Hort"]
 var deck_dict : Dictionary
+var card_skew = 0
 @onready var card_list_path = "res://assets/card_list.txt"
+@onready var card_template = preload("res://scenes/card.tscn")
 
 func _ready() -> void:
 	read_card_list()
-	$card.initialize(["Rat",1,1,"Crazy?","N/A","Mammal","Rat Bog","Common","Creature",0,"res://assets/cards/Rat.png"])
+
+func _physics_process(delta: float) -> void:
+	for i in range(len(deck)):
+		var random_card = deck.pick_random()
+		deck.erase(random_card)
+		instantiate_card(random_card)
+		print(deck)
+
+func instantiate_card(card_name):
+	var card_instance = card_template.instantiate()
+	$card_handler.add_child(card_instance)
+	card_instance.global_position = Vector3(-card_skew,0,-card_skew/2)
+	card_skew+=0.25
+	card_instance.initialize(deck_dict[card_name])
 
 func read_card_list():
 	var file = FileAccess.open(card_list_path,FileAccess.READ)
@@ -13,10 +28,5 @@ func read_card_list():
 	var line = text.split("\n")
 	for n in range(len(line)):
 		var words = line[n].split(';')
-		print(words)
-		#for i in range(len(words)):
-			#if words[i].is_valid_int():
-				#words[i] = int(words[i])
 		if words[0] in deck:
 			deck_dict[words[0]] = words
-	print(deck_dict)
--- isle/script/card.gd
+++ isle/script/card.gd
@@ -1,13 +1,16 @@
 extends Node3D
 var card_stats
+var health
+var damage
+var cost
 #0 - name - str
 #1 - base health - int
 #2 - base damage - int
 #3 - description - str
-#4 - ability name - str
+#4 - ability description - str
 #5 - type (bug etc.) - str
-#6 - location acquired - str
-#7 - rarity - str
+#6 - 2nd type (bug, etc.) - str
+#7 - ability name - str
 #8 - type (support, etc.) - str
 #9 - cost - int
 #10 - png name - string
@@ -16,11 +19,19 @@ func _physics_process(delta: float) -> void:
 
 func initialize(card_info):
 	card_stats = card_info
+	health = int(card_stats[1])
+	damage = int(card_stats[2])
+	cost = int(card_stats[9])
 	$name.text = card_info[0]
-	if card_info[1] != null:
-		$health_and_damage.text = str(card_info[1])+' : '+str(card_info[2])
+	if health != null:
+		$health_and_damage.text = str(health)+' : '+str(damage)
 	else:
 		$health_and_damage.text = "Support"
-	$cost.text = "Cost "+str(card_info[9])
-	$ability_and_description.text = card_info[3]
+	$cost.text = "Cost "+str(cost)
+	if card_info[4] != "N/A" and card_info[3] != "N/A":
+		$ability_and_description.text = card_info[3] + '\n' + card_info[4]
+	elif card_info[3] == "N/A":
+		$ability_and_description.text = card_info[4]
+	else:
+		$ability_and_description.text = card_info[3]
 	$card_mesh.get_surface_override_material(0).albedo_texture = load(card_info[10])
